add5aac866d1716048dec823efee97bcda992f21
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2026-05-31 12:58:04 +0200

    Reformat the canny file

--- ex_2/CannyEdgeDetector.py
+++ ex_2/CannyEdgeDetector.py
@@ -2,8 +2,19 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.ndimage import convolve
 
+#
+# NO MORE MODULES ALLOWED
+#
 
 def gaussFilter(img_in, ksize, sigma):
+    """
+    filter the image with a gauss kernel
+    :param img_in: 2D greyscale image (np.ndarray)
+    :param ksize: kernel size (int)
+    :param sigma: sigma (float)
+    :return: (kernel, filtered) kernel and gaussian filtered image (both np.ndarray)
+    """
+
     center = ksize // 2
     kernel = np.zeros((ksize, ksize))
     for x in range(ksize):
@@ -17,6 +28,14 @@ def gaussFilter(img_in, ksize, sigma):
 
 
 def sobel(img_in):
+    """
+    applies the sobel filters to the input image
+    Watch out! scipy.ndimage.convolve flips the kernel...
+
+    :param img_in: input image (np.ndarray)
+    :return: gx, gy - sobel filtered images in x- and y-direction (np.ndarray, np.ndarray)
+    """
+        
     Gx = np.array([[-1, 0, 1],
                    [-2, 0, 2],
                    [-1, 0, 1]])
@@ -29,12 +48,25 @@ def sobel(img_in):
 
 
 def gradientAndDirection(gx, gy):
+    """
+    calculates the gradient magnitude and direction images
+    :param gx: sobel filtered image in x direction (np.ndarray)
+    :param gy: sobel filtered image in x direction (np.ndarray)
+    :return: g, theta (np.ndarray, np.ndarray)
+    """
+        
     g = np.sqrt(gx**2 + gy**2).astype(int)
     theta = np.arctan2(gy, gx)
     return g, theta
 
 
 def convertAngle(angle):
+    """
+    compute nearest matching angle
+    :param angle: in radians
+    :return: nearest match of {0, 45, 90, 135}
+    """
+        
     deg = np.degrees(angle) % 180
     if deg < 22.5 or deg >= 157.5:
         return 0
@@ -47,6 +79,13 @@ def convertAngle(angle):
 
 
 def maxSuppress(g, theta):
+    """
+    calculate maximum suppression
+    :param g:  (np.ndarray)
+    :param theta: 2d image (np.ndarray)
+    :return: max_sup (np.ndarray)
+    """
+    
     rows, cols = g.shape
     result = np.zeros_like(g)
     for i in range(1, rows - 1):
@@ -66,6 +105,17 @@ def maxSuppress(g, theta):
 
 
 def hysteris(max_sup, t_low, t_high):
+    """
+    calculate hysteris thresholding.
+    Attention! This is a simplified version of the lectures hysteresis.
+    Please refer to the definition in the instruction
+
+    :param max_sup: 2d image (np.ndarray)
+    :param t_low: (int)
+    :param t_high: (int)
+    :return: hysteris thresholded image (np.ndarray)
+    """
+        
     rows, cols = max_sup.shape
     thresh = np.zeros_like(max_sup)
     thresh[max_sup > t_high] = 2
@@ -84,17 +134,46 @@ def hysteris(max_sup, t_low, t_high):
     return result
 
 
+
 def canny(img):
+    # gaussian
     kernel, gauss = gaussFilter(img, 5, 2)
+
+    # sobel
     gx, gy = sobel(gauss)
-    plt.subplot(1, 2, 1); plt.imshow(gx, 'gray'); plt.title('gx'); plt.colorbar()
-    plt.subplot(1, 2, 2); plt.imshow(gy, 'gray'); plt.title('gy'); plt.colorbar()
+
+    # plotting
+    plt.subplot(1, 2, 1)
+    plt.imshow(gx, 'gray')
+    plt.title('gx')
+    plt.colorbar()
+    plt.subplot(1, 2, 2)
+    plt.imshow(gy, 'gray')
+    plt.title('gy')
+    plt.colorbar()
     plt.show()
+
+    # gradient directions
     g, theta = gradientAndDirection(gx, gy)
-    plt.subplot(1, 2, 1); plt.imshow(g, 'gray'); plt.title('gradient magnitude'); plt.colorbar()
-    plt.subplot(1, 2, 2); plt.imshow(theta); plt.title('theta'); plt.colorbar()
+
+    # plotting
+    plt.subplot(1, 2, 1)
+    plt.imshow(g, 'gray')
+    plt.title('gradient magnitude')
+    plt.colorbar()
+    plt.subplot(1, 2, 2)
+    plt.imshow(theta)
+    plt.title('theta')
+    plt.colorbar()
     plt.show()
+
+    # maximum suppression
     maxS_img = maxSuppress(g, theta)
-    plt.imshow(maxS_img, 'gray'); plt.show()
+
+    # plotting
+    plt.imshow(maxS_img, 'gray')
+    plt.show()
+
     result = hysteris(maxS_img, 50, 75)
+
     return result
