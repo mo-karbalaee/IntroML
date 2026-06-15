f7ab2b3231460353895a6f0be6c4dceda5a0fc18
Mohammad Karbalaee Shabani <muhammad.ksht@gmail.com>
2026-06-15 21:21:20 +0200

    Learn more about binarization

--- ex_3/kaktovikAlignmentSimple.py
+++ ex_3/kaktovikAlignmentSimple.py
@@ -14,13 +14,45 @@ import cv2
 
 
 def _toGrayscale(img):
+    """
+    Converts the image to grayscale just in case.
+    We do this because for achieving a feature descriptor of the symbol, 
+    we only need the brightness values and dealing with three channels is not 
+    necessary for this task which is preprocessing for a classification task. 
+    """
     if len(img.shape) == 3:
         return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return img
 
 
 def _binarize(img):
+    """
+    We need to binarize the image because our goal is to distinguish the symbol 
+    inside each image and doing this is much easier with a binary image because 
+    the background will be removed and we only get the clean black pixels of the 
+    symbol itself. 
+    """
+    """
+    # Args
+    1. The image
+    2. The threshold which gets ignored here because we are sending the otsu flags
+    3. The maximum pixel value. 
+    4. The flags. Two flags have been passed. THRESH_OTSU specifies the thresholding 
+        algorithm for binarization. THRESH_BINARY_INV flag is about how we decide 
+        pixel values. This flag says, anything below the threshold should become white. 
+        That's the inverse part. The binary part specifies that the decision is a simple if 
+        statement because there are more complex ways of doing this as well. We make the symbol
+        pixels white because many techniques actually care for non-zero values not zero values
+        so dealing with zero values is not a common practice in image processing so we invert the 
+        image during the binarization. 
+    """
     _, binary = cv2.threshold(img, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    """
+    OpenCV's thresholding function returns two values, the threshold value that it 
+    computed during the Otsu thresholding, remember ex1?, and the binarized image itself.
+    Since we don't need the threshold value here, so after unpacking the function we 
+    threw away the threshold itself and only stored the binarized image. 
+    """
     return binary
 
 
